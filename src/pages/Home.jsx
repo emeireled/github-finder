98ccbb93ce6a832f9ccc5cf0bce3dda8f39a94cc
Edgar Meireles <edgar.meireles@cern.ch>
2023-01-06 16:50:09 +0100

    section 9 - 52

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <>
+      {/*Search Components*/}
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
